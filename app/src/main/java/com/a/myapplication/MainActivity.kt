84a3ccc4b1c3d67035230572d0f02a38e1b5836a
mouryaavadhesh <mourya.avadhesh@gmail.com>
2020-06-09 14:32:14 +0530

    code changes

--- app/src/main/java/com/a/myapplication/MainActivity.kt
+++ app/src/main/java/com/a/myapplication/MainActivity.kt
@@ -2,7 +2,6 @@ package com.a.myapplication
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -10,35 +9,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("onCreate", "onCreate")
     }
 
 
     override fun onStart() {
         super.onStart()
-        Log.i("onStart","onStart")
+        Log.i("onStart", "onStart")
     }
 
-    override fun onStop() {
-        super.onStop()
-        Log.i("onStart","onStart")
-
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.i("onRestart","onRestart")
-
+    override fun onResume() {
+        super.onResume()
+        Log.i("onResume", "onResume")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.i("onPause","onPause")
+        Log.i("onPause", "onPause")
+    }
 
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("onRestart", "onRestart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("onStart", "onStart")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.i("onDestroy","onDestroy")
-
+        Log.i("onDestroy", "onDestroy")
     }
 }
